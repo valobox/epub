f6336073f6a8b731c57207c7cfadad3d45ec5387
Oliver Brooks <oliver@completelynovel.com>
2012-07-18 15:43:25 +0100

    change css font size names to px and em values

--- lib/epub/item/css.rb
+++ lib/epub/item/css.rb
@@ -13,43 +13,6 @@ module Epub
     end
 
 
-    # Convert a css size rule value to ems, the supported formats are:
-    # * point (24pt)
-    # * pixels (24px)
-    # * percent (24%)
-    # * by name (xx-small, x-small, small, medium, large, x-large, xx-large)
-    #
-    # @param [String] css size rule
-    # @return [String] css size rule in ems
-    def self.val_to_em(rule_value)
-      # Convert CSS px values
-      rule_value = case rule_value        
-        when /xx-small/ then "9px"
-        when /x-small/  then "10px"
-        when /small/    then "13px"
-        when /medium/   then "16px"
-        when /large/    then "18px"
-        when /x-large/  then "24px"
-        when /xx-large/ then "32px"
-        else rule_value
-      end
-      
-      number = rule_value.gsub("[0-9.]+", "\\1")
-      
-      # Use multipliers to convert to ems
-      multiplier = case rule_value
-        when /[0-9.]+pt\s*$/ then 1.0/12
-        when /[0-9.]+px\s*$/ then 1.0/16
-        when /[0-9.]+em\s*$/ then 1
-        when /[0-9.]+%\s*$/  then 1.0/100
-        else return rule_value
-      end
-
-      amt = (multiplier.to_f * number.to_f)
-      return "%sem" % sprintf('%.2f', amt)
-    end
-
-
     # TODO: Split this up into multiple methods
     # Normalizes the guide by flattening the file paths
     # 
@@ -164,6 +127,32 @@ module Epub
           out << line
         end
         out
+      end
+
+
+      # Convert a css size rule value to ems, the supported formats are:
+      # * point (24pt)
+      # * pixels (24px)
+      # * percent (24%)
+      # * by name (xx-small, x-small, small, medium, large, x-large, xx-large)
+      #
+      # @param [String] css size rule
+      # @return [String] css size rule in ems
+      # see http://www.websemantics.co.uk/resources/font_size_conversion_chart/
+      def self.val_to_em(rule_value)
+        # Convert CSS names to px or em values
+        case rule_value
+          when /smaller/  then "0.8em"
+          when /larger/   then "1.2em"
+          when /xx-small/ then "8px"
+          when /x-small/  then "10px"
+          when /small/    then "13px"
+          when /medium/   then "16px"
+          when /large/    then "18px"
+          when /x-large/  then "24px"
+          when /xx-large/ then "32px"
+          else rule_value
+        end
       end
   end
 end
